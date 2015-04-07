d37398bd946de0de8f7b6e34544929466b8f6075
Tom Nijs <tom772@gmail.com>
2015-04-07 21:38:17 +0200

    Added screenfade

--- gamemode/dayCycle.lua
+++ gamemode/dayCycle.lua
@@ -1,9 +1,17 @@
 DAY_LENGTH = 10;
+FADE_LENGTH = 2;
 NIGHT_LENGTH = 2;
 
 function StartDay()
 
     timer.Simple(DAY_LENGTH, EndDay);
+    timer.Simple(DAY_LENGTH - FADE_LENGTH, FadePlayerScreens);
+end
+
+function FadePlayerScreens()
+    for k, ply in pairs(player.GetAll()) do
+        ply:ScreenFade(2, color_black, FADE_LENGTH, NIGHT_LENGTH);
+    end
 end
 
 function EndDay()
