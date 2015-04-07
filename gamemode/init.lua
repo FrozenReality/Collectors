ba7a626c45fc0c0a752a594648e4902c32d059a5
Diztro <Diztro.91@googlemail.com>
2015-04-07 20:57:38 +0100

    added new death messages

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -5,3 +5,11 @@ include("dayCycle.lua");
 function GM:Initialize()
     ServerLog('sweg');
 end
+
+function GM:PlayerDeath(victim, inflictor, attacker)
+	if (victim == attacker) then
+		PrintMessage( HUD_PRINTTALK, victim:Name() .. " commited suicide.")
+	else
+		PrintMessage( HUD_PRINTTALK, victim:Name() .. " was not in a safe zone")
+	end
+end
