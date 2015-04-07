c731d87fa5c14b03d51dd6cd8f5e65ebd1b98a27
Tom Nijs <tom772@gmail.com>
2015-04-07 21:30:36 +0200

    Added very basic dayCycle

---
+++ gamemode/dayCycle.lua
@@ -0,0 +1,18 @@
+DAY_LENGTH = 10;
+NIGHT_LENGTH = 2;
+
+function StartDay()
+    
+    timer.Simple(DAY_LENGTH, function()
+        EndDay();
+    end);
+end
+
+function EndDay()
+    for k, ply in pairs(player.GetAll()) do
+        ply:Kill();
+    end
+    timer.Simple(NIGHT_LENGTH, StartDay);
+end
+
+StartDay();
