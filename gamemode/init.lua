004a0896da0e5a69d30079020b45810f40a74be8
Diztro <Diztro.91@googlemail.com>
2015-04-07 22:57:00 +0100

    attempted to add a money system. (shouldn't work needs a database i think)

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,6 +1,35 @@
 AddCSLuaFile("shared.lua");
+AddCSLuaFile("sh_player.lua");
 
 include("dayCycle.lua");
+include("sh_player.lua");
+
+MONEY_STARTAMOUNT = 1000; --New players money amount.
+
+function FirstSpawn(ply)
+	local cash = ply:GetPData("money"); --Get the save money amount
+
+	if cash == nil then --If it doesn't exist supply the player with the starting money
+		ply:SetPData("money", MONEY_STARTAMOUNT); --Save it
+		ply:SetMoney(MONEY_STARTAMOUNT); --Set it to the networked ints that can be called from the client too
+	else
+		ply:SetMoney(cash); --If not, set the networked ints to what we last saved
+	end
+end
+hook.Add("PlayerInitialSpawn", "playerInitialSpawn", FirstSpawn);
+
+function PrintCash(pl)
+	pl:ChatPrint("Your cash is: " .. pl:GetMoney()); --Prints money amount
+end
+
+function fPlayerDisconnect(ply)
+	print("Player Disconnect: Money save to SQLLite and TXT");
+	ply:SaveMoney(); --Saves disconnected players money to SQL
+	ply:SaveMoneyTXT(); --Saves disconnected players money to TXT
+end
+
+concommand.Add("cash_get", PrintCash);
+
 
 function GM:Initialize()
     ServerLog('sweg');
@@ -8,8 +37,8 @@ end
 
 function GM:PlayerDeath(victim, attacker)
 	if (victim == attacker) then
-		PrintMessage( HUD_PRINTTALK, "You're a fucking faggot")
+		PrintMessage( HUD_PRINTTALK, "You're a fucking faggot");
 	else
-		PrintMessage( HUD_PRINTTALK,  "Test")
+		PrintMessage( HUD_PRINTTALK,  "Test");
 	end
 end
---
+++ gamemode/sh_player.lua
@@ -0,0 +1,28 @@
+local meta = FindMetaTable("Player"); --Get the meta table of player
+
+function meta:AddMoney(amount)
+	local current_cash = self:GiveMoney();
+end
+
+function meta:SetMoney(amount)
+	self:SetNetworkInt("Money", amount);
+	self:SaveMoney();
+end
+
+function meta:SaveMoney()
+	local cash = self:GetMoney();
+	self.SetPData("money", cash);
+end
+
+function SaveMoneyTXT()
+	file.Write(gmod.GetGameMode().Name .."/Money/".. string.gsub(self:SteamID(), ":", "_") ..".txt", self:GetMoneyString());
+end
+
+function meta:TakeMoney(amount)
+	--Add money function here
+	self:AddMoney(-amount);
+end
+
+function metaGetMoney(amount)
+	return self:GetNetworkedInt("Money");
+end
