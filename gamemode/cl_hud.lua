3f1cf4cdb113e014121d7876fd281c63be5fc6c7
Diztro <Diztro.91@googlemail.com>
2015-04-08 15:02:46 +0100

    no message

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,12 +1,16 @@
-[[ 
-local hide = {
-   CHudHealth = true,
-   CHudBattery = true,
-}
+function hud()
 
-hook.Add("HUDShouldDraw", "HideHUD", function(name))
-   if (hide[name]) then
-      return false
-   end
+local health = LocalPlayer():Health()
+draw.RoundedBox(0, 5, ScrH() - 15 - 20, health, 15, Color(255,0,0,255))
+draw.SimpleText(health, "default", 10, ScrH() - 15 - 40, Color(255,255,255,255))
+end 
+
+hook.Add("HUDPaint", "MyHudName", hud)
+ 
+function hidehud(name)
+	for k, v in pairs({"CHudHealth", "CHudBattery", "CHudAmmo", "CHudSecondaryAmmo"}) do
+		if name == v then return false end
+	end
 end
-]]
+
+hook.Add("HUDShouldDraw", "HideOurHud:D", hidehud)
